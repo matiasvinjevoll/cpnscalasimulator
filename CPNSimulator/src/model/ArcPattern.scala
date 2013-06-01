d22a104fa8bd100ed3102cf59aed0bf512d522b0
matiasvinjevoll <matiasvinjevoll@gmail.com>
2013-06-01 17:30:58 +0200

    Refactored.

--- CPNSimulator/src/model/ArcPattern.scala
+++ CPNSimulator/src/model/ArcPattern.scala
@@ -2,5 +2,4 @@ package model
 
 case class ArcPattern[Be <: BindingElement, A, Repr](
   arc: Arc[Be, A, Repr], pattern: Pattern[Be])
-  //(implicit ev1: Repr <:< Traversable[A], ev2: Repr => CPNColl[Repr])
   extends PatternBindingBasisObject[Be]
